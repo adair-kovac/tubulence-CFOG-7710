Q1 - modified calculations_from_intermediates.py to incorporate some fixes and implement w* and L. (There's an off-by-one-day error when I originally loaded the sonic data, hackfully fixing that at the end.) Autocorrelations - Performance optimizations, refactoring to run over real data.

AUTO_P4.py
@@ -1,74 +1,69 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from utils import path_util
+import datetime
+from data import data_loader
 
-U = np.random.normal(5, 1, 1000) # Insert 30-min U period here
-V = np.random.normal(5, 1, 1000) # Insert 30-min V period here
-W = np.random.normal(5, 1, 1000) # Insert 30-min W period here
-T = np.random.normal(5, 1, 1000) # Insert 30-min T period here
+def main():
+    U = np.random.normal(5, 1, 1000) # Insert 30-min U period here
+    V = np.random.normal(5, 1, 1000) # Insert 30-min V period here
+    W = np.random.normal(5, 1, 1000) # Insert 30-min W period here
+    T = np.random.normal(5, 1, 1000) # Insert 30-min T period here
 
-mean = sum(U)/len(U)
-auto_corr_lag_U = []
-auto_corr_denominator = sum((x-mean)**2 for x in U)
-dt = 1/20 # 20 Hz
-time = np.arange(0,len(U))
-time = time*dt
-#Assumption made: Data are sufficiently stationary (or homogeneous in space)
-for j in range(len(U)): # time lag
-    lag =[]
-    for k in range(0,len(U)-j):
-        index = (U[k]-mean)*(U[k+j]-mean)
-        lag.append(index)
-    auto_corr_lag_U.append(sum(lag)/auto_corr_denominator)
+    data = get_30_minute_data_window()
+    U = data["u"].to_numpy()
+    V = data["v"].to_numpy()
+    W = data["w"].to_numpy()
+    T = data["virtual_temp"].to_numpy()
 
-mean = sum(V)/len(V)
-auto_corr_lag_V = []
-auto_corr_denominator = sum((x-mean)**2 for x in V)
-dt = 1/20 # 20 Hz
-time = np.arange(0,len(V))
-time = time*dt
-#Assumption made: Data are sufficiently stationary (or homogeneous in space)
-for j in range(len(V)): # time lag
-    lag =[]
-    for k in range(0,len(V)-j):
-        index = (V[k]-mean)*(V[k+j]-mean)
-        lag.append(index)
-    auto_corr_lag_V.append(sum(lag)/auto_corr_denominator)
+    plot_autocorrelation(T, U, V, W)
 
-mean = sum(W)/len(W)
-auto_corr_lag_W = []
-auto_corr_denominator = sum((x-mean)**2 for x in W)
-dt = 1/20 # 20 Hz
-time = np.arange(0,len(W))
-time = time*dt
-#Assumption made: Data are sufficiently stationary (or homogeneous in space)
-for j in range(len(W)): # time lag
-    lag =[]
-    for k in range(0,len(W)-j):
-        index = (W[k]-mean)*(W[k+j]-mean)
-        lag.append(index)
-    auto_corr_lag_W.append(sum(lag)/auto_corr_denominator)
+def get_30_minute_data_window():
+    minutes = 30
+    frequency = 20 # Hz
+    num_observations = minutes * 60 * frequency
+    sonic_data = data_loader.load_processed_sonic_data(10, directory_override=True)
+    return sonic_data.iloc[0:num_observations]
 
-mean = sum(T)/len(T)
-auto_corr_lag_T = []
-auto_corr_denominator = sum((x-mean)**2 for x in T)
-dt = 1/20 # 20 Hz
-time = np.arange(0,len(T))
-time = time*dt
-#Assumption made: Data are sufficiently stationary (or homogeneous in space)
-for j in range(len(T)): # time lag
-    lag =[]
-    for k in range(0,len(T)-j):
-        index = (T[k]-mean)*(T[k+j]-mean)
-        lag.append(index)
-    auto_corr_lag_T.append(sum(lag)/auto_corr_denominator)
+def plot_autocorrelation(T, U, V, W):
+    print("starting autocorrelations")
+    auto_corr_lag_U = calculate_autocorrelation(U)
+    print("finished u")
+    auto_corr_lag_V = calculate_autocorrelation(V)
+    print("finished v")
+    auto_corr_lag_W = calculate_autocorrelation(W)
+    print("finished w")
+    auto_corr_lag_T = calculate_autocorrelation(T)
+    print("finished T")
+    dt = 1 / 20  # 20 Hz
+    time = np.arange(1, int(len(U)*dt))
 
-plt.plot(time,auto_corr_lag_U, label = "U velocity")
-plt.plot(time,auto_corr_lag_V, label = "V velocity")
-plt.plot(time,auto_corr_lag_W, label = "W velocity")
-plt.plot(time,auto_corr_lag_T, label = "Temperature")
-plt.ylabel('Autocorrelation')
-plt.xlabel("Lag (seconds)")
-plt.legend()
-plt.grid()
-plt.title("Autocorrelation vs. Lag")
-plt.show()
+    plt.plot(time, auto_corr_lag_U, label="U velocity")
+    plt.plot(time, auto_corr_lag_V, label="V velocity")
+    plt.plot(time, auto_corr_lag_W, label="W velocity")
+    plt.plot(time, auto_corr_lag_T, label="Virtual Temperature")
+    plt.ylabel('Autocorrelation')
+    plt.xlabel("Lag (seconds)")
+    plt.legend()
+    plt.grid()
+    plt.title("Autocorrelation vs. Lag")
+    plt.savefig(path_util.get_project_root() /
+                "AutocorrelationLag-{}.png".format(datetime.datetime.now()))
+
+
+def calculate_autocorrelation(variable):
+    mean = np.nanmean(variable)
+    auto_corr_lag = []
+    primes = variable - mean
+    auto_corr_denominator = np.nansum(np.float_power(primes, 2))
+    # Assumption made: Data are sufficiently stationary (or homogeneous in space)
+    for j in range(1, int(len(variable)/20)):  # time lag
+        skip = j*20
+        pairs = primes[skip:len(primes)]
+        lag = np.nansum(primes[0:len(primes)-skip]*pairs)
+        auto_corr_lag.append(lag / auto_corr_denominator)
+    return auto_corr_lag
+
+
+if __name__=="__main__":
+    main()

CFOG_Q1.py
@@ -19,9 +19,6 @@ def compute_std_with_start_times(start_time, column_name, A, Hz=20, dt=30):
     values = std_dev(A, Hz, dt)
     return create_dataframe(column_name, dt, start_time, values)
 
-def compute_prime_with_start_times(start_time, column_name, A, A_bar, Hz=20, dt=30):
-    values = compute_prime(A, A_bar, Hz=Hz, dt=dt)
-    return create_dataframe(column_name, dt, start_time, values)
 
 def create_dataframe(column_name, dt, start_time, values):
     count = 0
@@ -49,7 +46,8 @@ def compute_avg(A, Hz=20, dt=30):
         count = 0
         for j in range(start,end):
             if np.isnan(A[j]):
-                print("Skipping value at {}, NaN".format(j))
+               # print("Skipping value at {}, NaN".format(j))
+                pass
             else:
                 A_bar[i] = A_bar[i]+A[j]
                 count += 1